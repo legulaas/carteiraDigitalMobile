bc14355cb8129becd798e631593830cbb351348d
Matheus Fragoso <matheus.fragoso@dnafinanceiro.com>
2023-04-25 17:27:01 -0300

    v1.1.0 -> Criação de contas utilizando a classe Carteira.cs;

--- carteiraDigital/Class/Carteira.cs
+++ carteiraDigital/Class/Carteira.cs
@@ -3,90 +3,122 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Carteira
+
+namespace carteiraDigital.Class
 {
 
-	// Define variável 'Saldo', método set somente dentro da própria classe.
-	public double Saldo { get; private set; }
-
-	// Define variável 'Dono', métodos set e get;
-	public string Dono { get; set; }
-
-    /*
-     * Realiza saque de um valor da carteira digital
-     *
-     * @param double Valor
-     * @return boolean
-     */
-    public bool Sacar(double Valor)
+	public class Carteira
 	{
 
-		if(Valor > this.Saldo)
-		{
-			return false;
+		public double Saldo { get; private set; }
 
-		} else {
+		public string Dono { get; set; }
 
-			this.Saldo -= Valor;
-			return true;
+		public int Numero { get; set; }
 
-		}
+		public string CPF { get; set; }
 
-	}
+		public double Limite { get; set; }
 
-	/*
-	*
-	* Realiza deposito de um valor na carteira digital
-	*
-	* @param double Valor
-	* @return boolean
-	* 
-	*/
-	public bool Depositar (double Valor)
-	{
-		if(Valor > 0) {
-            this.Saldo += Valor;
-            return true;
-        } else {
-			return false;
-		}
+		public double Tarifa { get; private set; } = 19.90;
 
-	}
 
-	/*
-	 * 
-	 * Realiza transferência de uma carteira a outra
-	 * 
-	 * @param Carteira Destino
-	 * @param double Valor
-	 * @return boolean
-	*/ 
-	public bool Transferir (Carteira Destino, double Valor)
-	{	
-		// Verifica se há saldo para transferência
-		if(this.Saldo < Valor)
+		/*
+		 * Realiza saque de um valor da carteira digital
+		 *
+		 * @param double Valor
+		 * @return boolean
+		 */
+		public bool Sacar(double Valor)
 		{
 
-			return false;
-
-		} 
-		else
-		{	
-			// Realiza o saque de uma conta e o deposito em outra
-			bool saque = this.Sacar(Valor);
-			bool transferencia = Destino.Depositar(Valor);
-
-			// Verifica se a transferência foi realizada
-			if (transferencia)
+			if (Valor > this.Saldo)
 			{
-				return true;
-			} 
+				return false;
+
+			}
 			else
-			{	
-				// Deposita o valor da transferência caso haja falha
-				this.Depositar(Valor);
+			{
+
+				this.Saldo -= Valor;
+				return true;
+
+			}
+
+		}
+
+		/*
+		*
+		* Realiza deposito de um valor na carteira digital
+		*
+		* @param double Valor
+		* @return boolean
+		* 
+		*/
+		public bool Depositar(double Valor)
+		{
+			if (Valor > 0)
+			{
+				this.Saldo += Valor;
+				return true;
+			}
+			else
+			{
 				return false;
 			}
+
 		}
+
+		/*
+		 * 
+		 * Realiza transferência de uma carteira a outra
+		 * 
+		 * @param Carteira Destino
+		 * @param double Valor
+		 * @return boolean
+		*/
+		public bool Transferir(Carteira Destino, double Valor)
+		{
+			// Verifica se há saldo para transferência
+			if (this.Saldo < Valor)
+			{
+
+				return false;
+
+			}
+			else
+			{
+				// Realiza o saque de uma conta e o deposito em outra
+				bool saque = this.Sacar(Valor);
+				bool transferencia = Destino.Depositar(Valor);
+
+				// Verifica se a transferência foi realizada
+				if (transferencia)
+				{
+					return true;
+				}
+				else
+				{
+					// Deposita o valor da transferência caso haja falha
+					this.Depositar(Valor);
+					return false;
+				}
+			}
+		}
+
+		/*
+		 * 
+		 * Realiza a cobrança de tarifa mensalmente
+		 * 
+		 * @param int Dia
+		 * @return boolean
+		 * 
+		
+		public bool CobrarTarifa(int Dia)
+		{
+
+
+
+		}*/
 	}
 }
---
+++ carteiraDigital/Class/DadosCarteira.cs
@@ -0,0 +1,10 @@
+﻿
+namespace carteiraDigital.Class
+{
+    public class DadosCarteira
+    {
+        public List<Carteira> ListaDados = new List<Carteira>();
+
+        public DateTime DataSistema { get; set; } = DateTime.Now;
+    }
+}
