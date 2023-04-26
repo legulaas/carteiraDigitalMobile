18a4556854d190923994bfa5d2b35650e885e027
Matheus Fragoso <matheus.fragoso@dnafinanceiro.com>
2023-04-26 17:29:46 -0300

    v1.1.2 -> Depósito, início de criação da página de alteração de data do sistema.

--- carteiraDigital/Class/DadosCarteira.cs
+++ carteiraDigital/Class/DadosCarteira.cs
@@ -6,5 +6,22 @@ namespace carteiraDigital.Class
         public List<Carteira> ListaDados = new List<Carteira>();
 
         public DateTime DataSistema { get; set; } = DateTime.Now;
+
+        public DateTime HorarioInicial = DateTime.Today.AddHours(8);
+        public DateTime HorarioFinal = DateTime.Today.AddHours(18);
+
+
+        /*
+         * 
+         * Verifica se o horário do sistema está apto para realizar depósito, saque e transferências.
+         * 
+         * @return bool HorarioPermitido
+         * 
+         */
+        public bool VerificaHorario()
+        {
+            bool HorarioPermitido = DateTime.Compare(this.HorarioInicial, this.DataSistema) == -1 && DateTime.Compare(this.HorarioFinal, this.DataSistema) == 1 ? true : false;
+            return HorarioPermitido;
+        }
     }
 }
